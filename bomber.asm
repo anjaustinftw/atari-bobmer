224f95ac54f3255ed747fe2076112f59796fb50d
Aaron N. Josserand-Austin <anjaustinftw@gmail.com>
2021-04-04 14:14:47 -0600

    latest 04-APR-21

--- bomber.asm
+++ bomber.asm
@@ -5,7 +5,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
     include "vcs.h"
-    include "macros.h"
+    include "macro.h"
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Declare uninitialized variables
@@ -35,7 +35,6 @@ Reset:
 
     lda #10
     sta JetYPos             ; JetYPos = 10
-
     lda #60
     sta JetXPos             ; JetXPos = 60
 
@@ -47,22 +46,62 @@ StartFrame:
     lda #2
     sta VBLANK              ; Turn VBLANK on
     sta VSYNC               ; Turn VSYNC on
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Display 3 vertical scanlines of VSYNC
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
     REPEAT 3
         sta WSYNC
     REPEND
-
     lda #0
     sta VSYNC               ; Turn VSYNC off
-
     REPEAT 37
         sta WSYNC
     REPEND
-
     sta VBLANK              ; Turn VBLANK off
 
-    
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Display the 192 visible scanlines of our main game
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+GameVisibleLine:
+    lda #$84
+    sta COLUBK               ; set background/river color to blue
+    lda #$C2
+    sta COLUPF               ; set playfield/grass color to green
+    lda #%00000001
+    sta CTRLPF               ; enable playfield reflection
+    lda #$F0
+    sta PF0                  ; setting PF0 bit pattern
+    lda #$FC
+    sta PF1                  ; setting PF1 bit pattern
+    lda #0
+    sta PF2                  ; setting PF2 bit pattern
+
+    ldx #192                 ; X counts the number of remaining scanlines
+.GameLineLoop:
+    sta WSYNC
+    dex                      ; X--
+    bne .GameLineLoop        ; repeat next main game scanline until finished
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Display Overscan
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    lda #2
+    sta VBLANK               ; turn on VBLANK again
+    REPEAT 30
+        sta WSYNC            ; display 30 recommended lines of VBlank Overscan
+    REPEND
+    lda #0
+    sta VBLANK               ; turn off VBLANK
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Loop back to start a brand new frame
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    jmp StartFrame           ; continue to display the next frame
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Complete ROM size with exactly 4KB
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    org $FFFC                ; move to position $FFFC
+    word Reset               ; write 2 bytes with the program reset address
+    word Reset               ; write 2 bytes with the interruption vector
